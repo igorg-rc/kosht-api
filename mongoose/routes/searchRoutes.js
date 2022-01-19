e89d39d6d0f798bca3c7f1abe58b1d33edb4f2bd
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2022-01-19 10:34:29 +0200

    changed search routes file

--- mongoose/routes/searchRoutes.js
+++ mongoose/routes/searchRoutes.js
@@ -2,18 +2,30 @@ const router = require('express').Router()
 const Post = require('../models/Post')
 
 
+router.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ success: true, message: "Search endpoint" })
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error!" })
+  }
+})
+
 router.get("/:queryString", async (req, res) => {
-  const queryString = req.params.query
-  const allPosts = await Post.find()
-  if (!allPosts) return res.status(404).json({success: false, message: "No posts found!"})
-  const selectedPosts = allPosts.filter(item => (
-    (item.title).toLowerCase().includes(queryString.toLowerCase()) || 
-    (item.body).toLowerCase().includes(queryString.toLowerCase())  || 
-    (item.description).toLowerCase().includes(queryString.toLowerCase())
+  try {
+    const queryString = req.params.queryString
+    const allPosts = await Post.find()
+    if (!allPosts) return res.status(404).json({success: false, message: "No posts found!"})
+    const selectedPosts = allPosts.filter(item => (
+      (item.title).toLowerCase().includes(queryString.toLowerCase()) ||
+      (item.body).toLowerCase().includes(queryString.toLowerCase())  || 
+      (item.description).toLowerCase().includes(queryString.toLowerCase())
+      )
     )
-  )
-  if (!selectedPosts) return res.status(404).json({success: false, message: "No posts found!"})
-  res.status(200).json({success: true, data: selectedPosts})
+    if (!selectedPosts) return res.status(404).json({success: false, message: "No posts found!"})
+    res.status(200).json({success: true, data: selectedPosts})
+  } catch (error) {
+    return res.status(500).json({ message: "Server error occured!", error: error.message })
+  }
 })
 
 
